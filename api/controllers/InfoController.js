98cc120ba462566495c398e7d5fdd1ea5c03b609
Scott Cooper <scott.cooper2@mail.mcgill.ca>
2015-02-21 22:22:07 -0500

    Added info model

---
+++ api/controllers/InfoController.js
@@ -0,0 +1,11 @@
+/**
+ * InfoController
+ *
+ * @description :: Server-side logic for managing infoes
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
